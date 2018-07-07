37120626d12a6c599109d3c0df53f4ab34746915
Jose <jose.benitez@cern.ch>
2018-07-07 08:32:11 -0700

    latex

--- unix/latexpaper.sh
+++ unix/latexpaper.sh
@@ -3,8 +3,15 @@ if [ "$1" != "" ]; then
 export PAPER=$1
 fi
 
-#rm -f ${PAPER}.aux ${PAPER}.dvi ${PAPER}.log ${PAPER}.ps ${PAPER}.pdf
+# remove the file type
+echo $PAPER
+export PAPER=`echo ${PAPER} | sed -e 's/.tex//g'`
+echo $PAPER
+
+rm -f ${PAPER}.aux ${PAPER}.dvi ${PAPER}.log ${PAPER}.ps ${PAPER}.pdf
 
 pdflatex ${PAPER}.tex  
 #latex ${PAPER}.tex  
 #dvips ${PAPER}.dvi
+
+rm -f ${PAPER}.aux ${PAPER}.dvi ${PAPER}.log ${PAPER}.ps 
