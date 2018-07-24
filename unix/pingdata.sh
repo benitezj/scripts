0a9fae7eb44f132de381bc2ee05c64e4c738885f
Jose <jose.benitez@cern.ch>
2018-07-24 12:35:09 -0700

    compact pingdata

--- unix/pingdata.sh
+++ unix/pingdata.sh
@@ -5,5 +5,11 @@ echo "no ip"
 fi
 
 echo $IP > pingdata.log
+
 date >> pingdata.log
-ping -W 1000 $IP >> pingdata.log 
+
+ping -W 1000 -c 500 $IP >> pingdata.log 
+
+root -b pingdata.C
+
+open pingdata.png
